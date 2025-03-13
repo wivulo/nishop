4f4431cf463a8b7eae989abfe820dbcedd641fa3
vivulo <valtercio.ivulo@poligest.ao>
2025-03-13 12:31:41 +0100

    fix prisma migrations

---
+++ prisma/migrations/20250313113022_core/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageSrc" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "Emphasis" BOOLEAN NOT NULL,
+    "colors" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
